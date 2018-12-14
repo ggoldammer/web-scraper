92b0edb469a39a2f77e9390f278b1ef5b6975740
unknown <gaboguy@gmail.com>
2018-12-13 23:09:30 -0700

    initial commit

---
+++ scraper.js
@@ -0,0 +1,5 @@
+let express = require('express');
+let expbars = require('express-handlebars');
+let mongoose = require('mongoose');
+let cheerio = require('cheerio');
+let axios = require('axios');
