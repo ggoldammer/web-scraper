bf600ccb4c3306361d4795cc5bf2360eec341ff4
unknown <gaboguy@gmail.com>
2019-01-22 23:12:12 -0700

    Used in class to have base start

--- scraper.js
+++
@@ -1,5 +0,0 @@
-let express = require('express');
-let expbars = require('express-handlebars');
-let mongoose = require('mongoose');
-let cheerio = require('cheerio');
-let axios = require('axios');
